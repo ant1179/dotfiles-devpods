a1d15b974b0208f69de9c818e60d51c4ec35b1dd
ant <aneubert@mobol.net>
2025-01-12 21:34:23 +0100

    debug

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-DOTFILES_HOME=~/dotfiles
-
-# stow the config file from dotfiles into the container
-cd $DOTFILES_HOME
-rm -rf .git
-
-# set perl language settings
-export LC_CTYPE=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-
-cp -v $DOTFILES_HOME/zsh/.zshrc ~/.zshrc.bak
-rm ~/.zshrc
-cd $DOTFILES_HOME
-stow -t ~/ zsh
-stow -t ~/ nvim
+# DOTFILES_HOME=~/dotfiles
+#
+# # stow the config file from dotfiles into the container
+# cd $DOTFILES_HOME
+# rm -rf .git
+#
+# # set perl language settings
+# export LC_CTYPE=en_US.UTF-8
+# export LC_ALL=en_US.UTF-8
+#
+# cp -v $DOTFILES_HOME/zsh/.zshrc ~/.zshrc.bak
+# rm ~/.zshrc
+# cd $DOTFILES_HOME
+# stow -t ~/ zsh
+# stow -t ~/ nvim
 
 exit 0
