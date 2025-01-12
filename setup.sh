5dbf1f15f736d8f1776e44a8779984fa8138af28
ant <aneubert@mobol.net>
2025-01-12 19:04:50 +0100

    secured paths with variables

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+DOTFILES_HOME=~/dotfiles
+
 # stow the config file from dotfiles into the container
-cd ~/dotfiles
+cd $DOTFILES_HOME
 rm -rf .git
 
 # set perl language settings
@@ -9,10 +11,11 @@ export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
 rm ~/.zshrc
-cp -v zsh/.zshrc ~/.zshrc.bak
+cp -v $DOTFILES_HOME/zsh/.zshrc ~/.zshrc.bak
+cd $DOTFILES_HOME
 stow -t ~/ zsh
-source ~/.zshrc
+stow -t ~/ nvim
 
-stow nvim
+source ~/.zshrc
 
 cd
