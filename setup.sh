0c94f882ac59b52a5cb4a613c60a2f92bdd21408
ant <aneubert@mobol.net>
2025-01-12 21:48:30 +0100

    debug

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-# DOTFILES_HOME=~/dotfiles
-#
-# # stow the config file from dotfiles into the container
-# cd $DOTFILES_HOME
-# rm -rf .git
-#
-# # set perl language settings
-# export LC_CTYPE=en_US.UTF-8
-# export LC_ALL=en_US.UTF-8
-#
+DOTFILES_HOME=~/dotfiles
+
+# stow the config file from dotfiles into the container
+cd $DOTFILES_HOME
+rm -rf .git
+
+# set perl language settings
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
 # cp -v $DOTFILES_HOME/zsh/.zshrc ~/.zshrc.bak
 # rm ~/.zshrc
 # cd $DOTFILES_HOME
